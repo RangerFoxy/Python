366c24daccdf202fc29d04f840d4a02c205d586e
Dani Szabó <44240005+RangerFoxy@users.noreply.github.com>
2020-10-11 19:30:57 +0200

    Add files via upload

---
+++ 5/20140103a.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+
+def main():
+    print (ord('ߤ'))
+
+################################################################
+
+if __name__ == '__main__':
+    main()
