5645e17c2243293736c9a9463dcb25748fdb525f
Dani Szabó <44240005+RangerFoxy@users.noreply.github.com>
2020-10-16 12:39:31 +0200

    Add files via upload

---
+++ 5/20130218b.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+def valid(text, chars='ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'):
+    '''It returns a string, which contains the characters that are found in "chars"'''
+    li = [c for c in text]
+    li2 = [c for c in chars]
+    res = []
+    for c in li:
+        for ch in li2:
+            if ch == c:
+                res.append(c)
+    str = ''
+    return (str.join(res))
+
+def main():
+    print(valid('ASDk', '1234'))
+
+################################################################
+
+if __name__ == '__main__':
+    main()
---
+++ 5/20140103a.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+'''
+It prints 2020 to the console without any number in the source code.
+'''
+
+def main():
+    print (ord('ߤ'))
+
+################################################################
+
+if __name__ == '__main__':
+    main()
